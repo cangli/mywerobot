8895437c37ada9480c454075d0ade7e594808e99
gabrielhan <linhan@boyipricing.com>
2017-08-21 17:38:52 +0800

    sdf

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def hello_world():
 
 robot.config['APP_ID'] = 'wxf31a6c50bf4b7a07'
 robot.config['APP_SECRET'] = '9f513338d0611a633750afb981d20a5c'
+robot.config['ENCODING_AES_KEY'] = 'U4lCzGqRK0ExAsWoqjouEu4qO42qLLJkeDuMjauaVZg'
 robot.config['HOST'] = '0.0.0.0'
 robot.config['PORT'] = 80
 robot.run()
