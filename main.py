53563c3e6499b1aff1ae7837a39b23cf93b3bd5c
gabrielhan <linhan@boyipricing.com>
2017-08-21 17:16:25 +0800

    udpate

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17-8-21 下午5:12
+# @Author  : Aries
+# @Site    : 
+# @File    : main.py
+# @Software: PyCharm
+
+import werobot
+
+
+robot = werobot.WeRoBot(token='gabrielhan')
+
+
+@robot.text
+def hello_world():
+    return 'Hello World!'
+
+robot.run()
